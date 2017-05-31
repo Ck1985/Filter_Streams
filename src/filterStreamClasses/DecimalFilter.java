50a9aea8f6e19a160907a0e90e8cc70f779c7345
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-31 22:24:13 +0700

    New Commit

--- src/filterStreamClasses/DecimalFilter.java
+++ src/filterStreamClasses/DecimalFilter.java
@@ -17,6 +17,22 @@ public class DecimalFilter extends DumpFilter {
         buf = new int[ratio];
         int datum = in.read();
         this.numRead++;
-
+        if (datum == -1) {
+        }
+        String decimal = Integer.toString(datum);
+        if (datum < 10) {
+            decimal = "00" + decimal;
+        } else if (datum < 100) {
+            decimal = "0" + decimal;
+        }
+        for (int i = 0; i < decimal.length(); i++) {
+            buf[i] = decimal.charAt(i);
+        }
+        if (numRead < breakAfter) {
+            buf[buf.length - 1] = ' ';
+        } else {
+            buf[buf.length - 1] = '\n';
+            numRead = 0;
+        }
     }
 }
