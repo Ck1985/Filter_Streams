b949f4aed63a4880e6515ca62a03a5627bbcd022
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-06-08 11:49:03 +0700

    New Commit

--- src/dataStreams/IntReader.java
+++ src/dataStreams/IntReader.java
@@ -1,7 +1,27 @@
 package dataStreams;
 
+import java.io.*;
+
 /**
  * Created by anonymous.vn1985@gmail.com
  */
 public class IntReader {
+    public static void main(String[] args) throws IOException {
+        DataInputStream dis = null;
+        try {
+            FileInputStream fis = new FileInputStream(args[0]);
+            System.out.println("------------" + args[0] + "-------------");
+            dis = new DataInputStream(fis);
+
+            int result;
+            while (true) {
+                result = dis.readInt();
+                System.out.print(result + " ");
+            }
+        } catch (EOFException ex) {
+            dis.close();
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
 }
