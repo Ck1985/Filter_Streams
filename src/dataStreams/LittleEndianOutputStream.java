60e09b5c3b37a16461c67ed3ad53d9fe43b916e9
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-06-09 17:24:52 +0700

    New Commit

--- src/dataStreams/LittleEndianOutputStream.java
+++ src/dataStreams/LittleEndianOutputStream.java
@@ -28,31 +28,31 @@ public class LittleEndianOutputStream extends FilterOutputStream {
         this.written++;
     }
     public void writeShort(int v) throws IOException {
-        out.write((v >>> 8) & 0xFF);
         out.write((v >>> 0) & 0xFF);
+        out.write((v >>> 8) & 0xFF);
         this.written += 2;
     }
     public void writeChar(int v) throws IOException {
-        out.write((v >>> 8) & 0xFF);
         out.write((v >>> 0) & 0xFF);
+        out.write((v >>> 8) & 0xFF);
         this.written += 2;
     }
     public void writeInt(int v) throws IOException {
-        out.write((v >>> 24) & 0xFF);
-        out.write((v >>> 16) & 0xFF);
-        out.write((v >>> 8) & 0xFF);
         out.write((v >>> 0) & 0xFF);
+        out.write((v >>> 8) & 0xFF);
+        out.write((v >>> 16) & 0xFF);
+        out.write((v >>> 24) & 0xFF);
         this.written += 4;
     }
     public void writeLong(long v) throws IOException {
-        out.write((int)(v >>> 56) & 0xFF);
-        out.write((int)(v >>> 48) & 0xFF);
-        out.write((int)(v >>> 40) & 0xFF);
-        out.write((int)(v >>> 32) & 0xFF);
-        out.write((int)(v >>> 24) & 0xFF);
-        out.write((int)(v >>> 16) & 0xFF);
-        out.write((int)(v >>> 8) & 0xFF);
         out.write((int)(v >>> 0) & 0xFF);
+        out.write((int)(v >>> 8) & 0xFF);
+        out.write((int)(v >>> 16) & 0xFF);
+        out.write((int)(v >>> 24) & 0xFF);
+        out.write((int)(v >>> 32) & 0xFF);
+        out.write((int)(v >>> 40) & 0xFF);
+        out.write((int)(v >>> 48) & 0xFF);
+        out.write((int)(v >>> 56) & 0xFF);
         this.written += 8;
     }
     public void writeFloat(float v) throws IOException {
@@ -61,4 +61,40 @@ public class LittleEndianOutputStream extends FilterOutputStream {
     public void writeDouble(double v) throws IOException {
         this.writeLong(Double.doubleToLongBits(v));
     }
+    public void writeBytes(String s) throws IOException {
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            out.write((byte)s.charAt(i));
+        }
+        this.written += length;
+    }
+    public void writeChars(String s) throws IOException {
+        int length = s.length();
+        int n = 0;
+        for (int i = 0; i < length; i++) {
+            n = s.charAt(i);
+            out.write((n >>> 0) & 0xFF);
+            out.write((n >>> 8) & 0xFF);
+            this.written += 2;
+        }
+    }
+    public void writeUTF(String  s) throws IOException {
+        int numChars = s.length();
+        int numBytes = 0;
+        int c = 0;
+        for (int i = 0; i < numChars; i++) {
+            c = s.charAt(i);
+            if ((c >= 0x0001) && (c <= 0x007F)) numBytes++;
+            else if (c > 0x07FF) numBytes += 3;
+            else numBytes += 2;
+        }
+        if (numBytes > 65535) throw new UTFDataFormatException();
+         out.write((numBytes >>> 8) & 0xFF);
+         out.write((numBytes >>> 0) & 0xFF);
+         for (int i = 0; i < numChars; i++) {
+             c = s.charAt(i);
+             if ((c >= 0x0001) && (c <= 0x007F))
+         }
+    }
+
 }
